8485410380d93a04546e268e7cb5ff67788a9267
Nabin Karki Thapa <nabin@outlook.com>
2018-03-25 20:08:03 +1100

    file deleted

--- sample/NToastNotify.Web.FeatureFolders/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NToastNotify.Noty.FeatureFolders.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
