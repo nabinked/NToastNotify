34e8402176a896324c394903065eaf2fde74b25d
Humayun Shoukat <humayun@deltashoppe.com>
2019-02-19 19:11:53 +0500

    Hotfix SimpleInjector NullReference Exception
    When using simple injector as DI i got the null reference exception when the SimpleInjector calls its verify method. Reason for the error is this line
    
     MessageContainer = messageContainerFactory.Create<TMessage>();
    
    in the constructor of ToastNotification class, because the MessageContainerFactory.Create Method depends on HttpContext which is null at the time of verify called mad by DI. The propsed changes fix this issue.

--- src/ToastNotification.cs
+++ src/ToastNotification.cs
@@ -8,11 +8,12 @@ namespace NToastNotify
         where TMessage : class, IToastMessage
         where TOptions : class, ILibraryOptions, new()
     {
+        private readonly IMessageContainerFactory _messageContainerFactory;
         protected IMessageContainer<TMessage> MessageContainer;
 
         protected ToastNotification(IMessageContainerFactory messageContainerFactory)
         {
-            MessageContainer = messageContainerFactory.Create<TMessage>();
+            _messageContainerFactory = messageContainerFactory;
         }
         public abstract void AddAlertToastMessage(string message = null, ILibraryOptions toastOptions = null);
         public abstract void AddErrorToastMessage(string message = null, ILibraryOptions toastOptions = null);
@@ -21,23 +22,29 @@ namespace NToastNotify
         public abstract void AddWarningToastMessage(string message = null, ILibraryOptions toastOptions = null);
         public IEnumerable<IToastMessage> GetToastMessages()
         {
-            return MessageContainer.GetAll();
+            return MessageContainer?.GetAll() ?? new List<TMessage>(); ;
         }
 
         public IEnumerable<IToastMessage> ReadAllMessages()
         {
-            return MessageContainer.ReadAll();
+            return MessageContainer?.ReadAll() ?? new List<TMessage>(); ;
         }
 
         public void RemoveAll()
         {
-            MessageContainer.RemoveAll();
+            MessageContainer?.RemoveAll();
         }
 
         protected void AddMessage(TMessage toastMessage)
         {
             OptionsHelpers.EnsureSameType<TOptions>(toastMessage.Options);
+            InitializeMessageContainer();
             MessageContainer.Add(toastMessage);
+        }
+
+        private void InitializeMessageContainer()
+        {
+            MessageContainer = MessageContainer ?? _messageContainerFactory.Create<TMessage>();
         }
     }
 }
