e1eb3bcc7db4e146482f7f4572c794dc04c0e76a
Nabin Karki Thapa <nabin@outlook.com>
2020-07-09 17:52:58 +1000

    add form post example

---
+++ samples/Toastr/Pages/FormPost.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NToastNotify;
+
+namespace Toastr.Pages
+{
+    public class FormPostModel : PageModel
+    {
+        private readonly IToastNotification _toastNotification;
+
+        public FormPostModel(IToastNotification toastNotification)
+        {
+            _toastNotification = toastNotification;
+        }
+        public void OnGet()
+        {
+
+        }
+        public IActionResult OnPost(string input, string redirect)
+        {
+            _toastNotification.AddSuccessToastMessage("Input received: " + input);
+            if (redirect == "on")
+                return RedirectToPage("/Privacy");
+            return Page();
+        }
+    }
+}
