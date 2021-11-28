1fb9a4d9a09993fdcd9c17340e88225b227daf6a
nabin <nabin@outlook.com>
2021-11-29 00:10:56 +1100

    add version on sammple notification

--- samples/Noty/Pages/Index.cshtml.cs
+++ samples/Noty/Pages/Index.cshtml.cs
@@ -1,22 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using NToastNotify;
 
 namespace Noty.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly string _version;
         private readonly IToastNotification _toastNotification;
         //private readonly NotyNotification _toastNotification;
 
         public IndexModel(IToastNotification toastNotification)
         {
+            _version = Assembly.GetAssembly(typeof(IToastNotification))?.GetName().Version?.ToString() ?? throw new Exception("Version not found");
             _toastNotification = toastNotification;
             //_toastNotification = toastNotification as NotyNotification;
         }
         public void OnGet()
         {
             _toastNotification.AddSuccessToastMessage();
-            _toastNotification.AddErrorToastMessage("Test m'e'ssag'e with single q'oute", new NotyOptions()
+            _toastNotification.AddErrorToastMessage("Test m'e'ssag'e with single q'oute. Version: " + _version, new NotyOptions()
             {
                 Timeout = 0,
                 Layout = "topLeft",
@@ -29,20 +33,20 @@ namespace Noty.Pages
             {
                 Layout = "bottomLeft"
             });
-            _toastNotification.AddInfoToastMessage("This is an info toast", new NotyOptions()
+            _toastNotification.AddInfoToastMessage("This is an info toast. Version: " + _version, new NotyOptions()
             {
                 ProgressBar = false,
                 Layout = "center"
             });
 
             //Warning
-            _toastNotification.AddWarningToastMessage("Waring go bottom right", new NotyOptions()
+            _toastNotification.AddWarningToastMessage("Waring go bottom right. Version: " + _version, new NotyOptions()
             {
                 Layout = "bottomRight"
             });
 
             //Error
-            _toastNotification.AddErrorToastMessage("Custom Error Message", new NotyOptions() { Theme = "mint" });
+            _toastNotification.AddErrorToastMessage("Custom Error Message. Version: " + _version, new NotyOptions() { Theme = "mint" });
         }
     }
 }
