e20718c03120f632212d9b2a10ceaf8686eeade0
Ali Bahari <AliBahariDotNet@GMail.com>
2021-12-23 14:25:19 +0330

    Added Support of Generic Toast Libraries. (Like DevExtreme Toasts) - Sample Project

---
+++ samples/GenericDevExtreme/DevExtremeToastOptions.cs
@@ -0,0 +1,12 @@
+﻿using NToastNotify;
+using NToastNotify.Helpers;
+
+namespace GenericDevExtreme
+{
+    public class DevExtremeToastOptions : GenericOptions
+    {
+        public int DisplayTime { get; set; }
+
+        public override string Json => this.ToJson();
+    }
+}
